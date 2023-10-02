058d71385aa88595ff84352520578692f3da35b5
nandaafd <nandaafd.info@gmail.com>
2023-10-03 00:11:53 +0700

    updating data

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ExportController;
 use App\Http\Controllers\ProdukController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,5 +28,21 @@ Route::middleware(['auth'])->group(function (){
     Route::post('logout', [AuthController::class, 'logout']);
     Route::resource('produk', ProdukController::class);  
     Route::get('download', [ExportController::class, 'export']);
+});
+Route::get('migrate-first', function () {
+    Artisan::call('migrate --path=database/migrations/2023_09_30_093118_create_kategoris_table.php');
+    return 'success';
+});
+Route::get('migrate-sec', function () {
+    Artisan::call('migrate');
+    return 'success';
+});
+Route::get('seed-first', function () {
+    Artisan::call('db:seed --class=UserSeeder');
+    return 'success';
+});
+Route::get('seed-sec', function () {
+    Artisan::call('db:seed --class=KategoriSeeder');
+    return 'success';
 });
 
