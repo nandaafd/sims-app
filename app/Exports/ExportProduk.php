f5c1ca5e2f122b77f02a180648b5a42fb455a7ac
nandaafd <nandaafd.info@gmail.com>
2023-10-03 19:11:17 +0700

    improve export excel

--- app/Exports/ExportProduk.php
+++ app/Exports/ExportProduk.php
@@ -8,15 +8,21 @@ use Illuminate\Contracts\View\View;
 
 class ExportProduk implements FromView
 {
-    protected $filter;
+    protected $filter1;
+    protected $filter2;
 
-    public function __construct($filter)
+    public function __construct($filter1, $filter2)
     {
-        $this->filter = $filter;
+        $this->filter1 = $filter1;
+        $this->filter2 = $filter2;
     }
-    public function view():View
+
+    public function view(): View
     {
-        $item = Produk::where('kategori_id', $this->filter)->get();
-        return view('produk.table', ['item'=>$item]);   
-    }
+        // Menggunakan kedua filter dalam query
+        $item = Produk::where('kategori_id', $this->filter1)
+                       ->where('nama_produk', $this->filter2)
+                       ->get();
+                       return view('produk.table', ['item' => $item]);
+    }  
 }
--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -10,10 +10,10 @@ use Maatwebsite\Excel\Facades\Excel;
 class ExportController extends Controller
 {
     public function export(Request $request){
-        $filter = $request->input('kategori');
-
+        $filter1 = $request->input('kategori');
+        $filter2 = $request->input('cari_nama');
         // Buat instance dari InvoicesExport dengan filter
-        $export = new ExportProduk($filter);
+        $export = new ExportProduk($filter1, $filter2);
 
         // Ekspor data ke Excel
         return Excel::download($export, 'produk.xlsx');
